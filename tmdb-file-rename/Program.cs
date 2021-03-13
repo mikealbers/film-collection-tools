960185f4482b4bfd5c737e13407e886b0122fb52
mike albers <malbears@gmail.com>
2021-03-12 16:36:41 -0800

    init

---
+++ tmdb-file-rename/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tmdb_file_rename
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Please provide a TMDB api key");
+            }
+            else 
+            {
+                Console.WriteLine(args[1]);
+            }
+
+        }
+    }
+}
