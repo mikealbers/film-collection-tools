5c469412501eb1f7a3f6e80b9aa19f24adb7d996
mike albers <malbears@gmail.com>
2021-03-16 14:42:40 -0700

    add tool to create csv from formatted file names

---
+++ files-to-letterboxd-csv/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace files_to_letterboxd_csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"E:/Cinema/E-ManualRename";
+            string fileName = Path.GetFileNameWithoutExtension(directoryPath);
+
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+            FileInfo[] files = directory.GetFiles();
+
+            string csv = "Title,Year," + Environment.NewLine;
+
+            Console.WriteLine(directory.FullName);
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(Path.GetFileNameWithoutExtension(file.FullName));
+                string[] titleYearSplit = Path.GetFileNameWithoutExtension(file.FullName).Split('(');
+                string title = titleYearSplit[0].TrimEnd(' ').Replace(",", "");
+                string year = titleYearSplit[1].Replace("(", "").Replace(")", "");
+                Console.WriteLine("After the split");
+                csv += title + "," + year + "," + Environment.NewLine;
+            }
+            File.WriteAllText(directoryPath + @"\1-" + fileName + "-letterboxd-list.csv", csv);
+        }
+    }
+}
